0539b476e9e8ad1246de84846ef6f11cad7a7a4a
jeraldpraveen <jerald.vit@gmail.com>
2022-05-01 22:08:57 +0530

    16.Get Input From Users

--- 02-notes-app/app.js
+++ 02-notes-app/app.js
@@ -4,5 +4,13 @@ const chalk = require("chalk");
 const msg = getNotes();
 console.log(chalk.blue.inverse(msg));
 
-// [ GET INPUT FROM TERMINAL ]---> node app.js Jerald
+// [ GET INPUT FROM TERMINAL ]---> node app.js add
+// [ GET INPUT FROM TERMINAL ]---> node app.js remove
+const command = process.argv[2];
+
+if (command === "add") {
+  console.log("Adding Note");
+}
+
+// [ GET INPUT FROM TERMINAL ]---> node app.js add --title='New Title'
 console.log(process.argv);
