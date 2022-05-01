5ec9d9876ee9ad7d0e8eefbb92696594851d3954
jeraldpraveen <jerald.vit@gmail.com>
2022-05-01 15:11:29 +0530

    9.Importing 3rd Party npm modules

--- 01-notes-app/app.js
+++ 01-notes-app/app.js
@@ -1,8 +1,10 @@
 const nameTwo = require("./utils");
 const sum = require("./utils-one");
+const validator = require("validator");
 
 const nameOne = "Jerald";
 
 const sumResult = sum(4, 2);
+const isEmail = validator.isEmail("jerald@gmail.com");
 
-console.log(nameOne, nameTwo, sumResult);
+console.log(nameOne, nameTwo, sumResult, isEmail);
